699ba433d70589ae8ce0cea34d87ae085495808e
Sarah Ohlin <sarahohlin1@gmail.com>
2020-07-27 19:59:33 -0400

    feat: vue store

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,13 +1,16 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Vuex from 'vuex';
 
 import router from './router';
+import store from './store';
 
 Vue.use(VueRouter);
 
 const vm = new Vue({
 	el: '#root',
 	router,
+	store,
 	template: '<router-view />'
 });
 
