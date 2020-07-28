9e122de04ef37abb2af34760934d05fb8512a87b
Sarah Ohlin <sarahohlin1@gmail.com>
2020-07-27 21:51:28 -0400

    feat: app bar

--- src/frontend/views/Home.vue
+++ src/frontend/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
-<h1>home</h1>
+<v-app>
+	<v-app-bar elevate fixed color="primary">
+		<v-app-bar-nav-icon />
+		<v-img class="clickable mr-4" @click="toSteamPage()" max-width="40" contain alt="logo" src="/assets/mr-logo.webp" />
+		<v-toolbar-title color="white">marble race community boards</v-toolbar-title>
+		<v-spacer />
+	</v-app-bar>
+</v-app>
 </template>
 
 <script>
@@ -7,6 +14,11 @@ export default {
 	name: 'Home',
 	mounted() {
 		document.title = "MRBoard";
+	},
+	methods: {
+		toSteamPage() {
+			document.location = 'https://store.steampowered.com/app/851640/Marble_Race/';
+		}
 	}
 };
 </script>
@@ -14,5 +26,11 @@ export default {
 <style lang="scss" scoped>
 
 @use '~@/common';
+
+.clickable {
+	&:hover {
+		cursor: pointer;
+	}
+}
 
 </style>
--- src/frontend/views/Page404.vue
+++ src/frontend/views/Page404.vue
@@ -4,13 +4,20 @@
 		<v-row align-content="center" justify="center">
 			<v-col>
 				<h1 class="text-center">
-					404 Not Found :(
+					oh nO ;A;
 				</h1>
 			</v-col>
 		</v-row>
 		<v-row align-content="center" justify="center">
 			<v-col class="text-center">
-				<v-btn color="primary" @click="goHome()">go home</v-btn>
+				<h4 class="text-center">
+					...this page doesn't exist :(
+				</h4>
+			</v-col>
+		</v-row>
+		<v-row align-content="center" justify="center">
+			<v-col class="text-center">
+				<v-btn color="primary" @click="goHome()"><v-icon>mdi-arrow-left</v-icon>go home</v-btn>
 			</v-col>
 		</v-row>
 	</v-container>
@@ -21,7 +28,7 @@
 export default {
 	name: 'Page404',
 	mounted() {
-		document.title = "404 - Page not found.";
+		document.title = "404 ;A;";
 	},
 	methods: {
 		goHome() {
