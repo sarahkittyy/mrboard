b24409c417667745d9cbcf1ccddee5a6b2985290
Sarah Ohlin <sarahohlin1@gmail.com>
2020-07-27 20:57:06 -0400

    feat: 404 page

--- src/frontend/views/Page404.vue
+++ src/frontend/views/Page404.vue
@@ -1,6 +1,19 @@
 <template>
 <v-app>
-	404
+	<v-container class="ma-auto">
+		<v-row align-content="center" justify="center">
+			<v-col>
+				<h1 class="text-center">
+					404 Not Found :(
+				</h1>
+			</v-col>
+		</v-row>
+		<v-row align-content="center" justify="center">
+			<v-col class="text-center">
+				<v-btn @click="goHome()">go home</v-btn>
+			</v-col>
+		</v-row>
+	</v-container>
 </v-app>
 </template>
 
@@ -9,6 +22,11 @@ export default {
 	name: 'Page404',
 	mounted() {
 		document.title = "404 - Page not found.";
+	},
+	methods: {
+		goHome() {
+			this.$router.push('/');
+		}
 	}
 };
 </script>
