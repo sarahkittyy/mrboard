decde196e1671690fc8fffc09b31e209335a3761
Sarah Ohlin <sarahohlin1@gmail.com>
2020-07-29 01:39:34 -0400

    refactor: TimeController

---
+++ src/backend/api/db/controllers/TimeController.ts
@@ -0,0 +1,109 @@
+import express, { Request, Response } from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
+import { TimeModel, Time } from '../models/Time';
+import { UserModel } from '../models/User';
+import { LevelModel } from '../models/Level';
+
+import replayParser, { Replay } from '../../../util/replayParser';
+
+import { getThumbnailURL } from '../../../util/steam';
+import { UploadedFile } from 'express-fileupload';
+
+export class TimeController {
+	/**
+	 * get all times
+	 */
+	public static all = async (req: Request, res: Response) => {
+		return res.send(await TimeModel.find({})
+			.populate('level')
+			.populate('author')
+			.lean());
+	}
+	
+	/**
+	 * get all of the currently logged in user's times
+	 */
+	public static mine = async (req: Request, res: Response) => {
+		let userId = req.user.steam_id;
+		let user = await UserModel.getUser(userId);
+		
+		return res.send({ times: user.times ?? [] });
+	}
+	
+	/**
+	 * get all times of a specific user
+	 */
+	public static users = async (req: Request, res: Response) => {
+		let user = await UserModel.findOne({
+			_id: req.params.user
+		}, 'times')
+		.populate('times')
+		.lean();
+		
+		if (!user) {
+			return res.status(404).send(`Could not find user ${req.params.user}`);
+		}
+		
+		return res.send({ times: user.times ?? [] });
+	}
+	
+	/**
+	 * post a new time
+	 */
+	public static new = async (req: Request, res: Response) => {
+		const rpl = req.files.rpl as UploadedFile;
+		let data: Replay;
+		try {
+			data = replayParser(rpl.tempFilePath);
+		} catch (err) {
+			return res.status(400).send(`Could not parse .rpl file - ${err}`);
+		}
+		
+		// try to find the associated level
+		let level = await LevelModel.findOne({ steam_id: data.Id.toString(), name: data.Level });
+		if (!level) {
+			// add this level to the db
+			level = new LevelModel();
+			level.steam_id = data.Id.toString();
+			level.name = data.Level;
+			level.campaign = data.Campaign;
+			try {
+				level.thumbnailURL = await getThumbnailURL(data.Id.toString());
+			} catch (err) {
+				return res.status(500).send('Internal server error.');
+			}
+			level.save();
+		}
+		
+		// save the replay file
+		let filename: string = `storage/${uuidv4()}.rpl`;
+		rpl.mv(filename);
+		
+		// get the author
+		let author = await UserModel.getUser(req.user.steam_id);
+		
+		// see if the time exists
+		let time = await TimeModel.findOne({
+			level: level._id,
+			author: author._id,
+		});
+		if (!time) {
+			time = new TimeModel();
+			time.level = level;
+			time.author = author;
+		}
+		time.duration = data.time;
+		time.timestamp = new Date(data.Timestamp * 1000);
+		time.verified = false;
+		time.replay = filename;
+		time.save();
+		
+		level.times.push(time);
+		author.times.push(time);
+		level.save();
+		author.save();
+
+		return res.send('Success');
+	}
+};
--- src/backend/api/times.ts
+++ src/backend/api/times.ts
@@ -1,128 +1,25 @@
-import express, { Request, Response } from 'express';
-import { param, body, oneOf } from 'express-validator';
-import passport from 'passport';
-import { v4 as uuidv4 } from 'uuid';
-import appRoot from 'app-root-path';
+import express from 'express';
+import { param } from 'express-validator';
 
 import assert from './middleware/assert';
 import requireAuth from './middleware/requireAuth';
 
-import { TimeModel } from './db/models/Time';
-import { UserModel } from './db/models/User';
-import { LevelModel } from './db/models/Level';
-
-import replayParser, { Replay } from '../util/replayParser';
 import isReplayFile from './middleware/isReplayFile';
 
-import { User } from './db/models/User';
-import { UploadedFile } from 'express-fileupload';
-
-import { getThumbnailURL } from '../util/steam';
+import { TimeController } from './db/controllers/TimeController'; 
 
 const times = express.Router();
 
-/**
- * get all times
- */
-times.get('/', async (req, res) => {
-	return res.send(await TimeModel.find({})
-		.populate('level')
-		.populate('author')
-		.lean());
-});
- 
-/**
- * get all of the currently authenticated user's times
- */
-times.get('/me', requireAuth, async (req, res) => {
-	let userId = req.user.steam_id;
-	let user = await UserModel.getUser(userId);
-	
-	return res.send({ times: user.times ?? [] });
-});
- 
-/**
- * get all of the provided user's times
- */
+times.get('/', TimeController.all);
+times.get('/me', requireAuth, TimeController.mine);
 times.get('/:user', [
 	param('user').isNumeric(),
 	assert
-], async (req: Request, res: Response) => {
-	let user = await UserModel.findOne({
-		_id: req.params.user
-	}, 'times')
-	.populate('times')
-	.lean();
-	
-	if (!user) {
-		return res.status(404).send(`Could not find user ${req.params.user}`);
-	}
-	
-	return res.send({ times: user.times ?? [] });
-});
-
-/**
- * post a new time
- * 
- */
+], TimeController.users);
 times.post('/new', [
 	requireAuth,
 	isReplayFile('rpl'),
 	assert
-], async (req: Request, res: Response) => {
-	const rpl = req.files.rpl as UploadedFile;
-	let data: Replay;
-	try {
-		data = replayParser(rpl.tempFilePath);
-	} catch (err) {
-		return res.status(400).send(`Could not parse .rpl file - ${err}`);
-	}
-	
-	// try to find the associated level
-	let level = await LevelModel.findOne({ steam_id: data.Id.toString(), name: data.Level });
-	if (!level) {
-		// add this level to the db
-		level = new LevelModel();
-		level.steam_id = data.Id.toString();
-		level.name = data.Level;
-		level.campaign = data.Campaign;
-		try {
-			level.thumbnailURL = await getThumbnailURL(data.Id.toString());
-		} catch (err) {
-			return res.status(500).send('Internal server error.');
-		}
-		level.save();
-	}
-	
-	// save the replay file
-	let filename: string = `storage/${uuidv4()}.rpl`;
-	rpl.mv(filename);
-	
-	// get the author
-	let author = await UserModel.getUser(req.user.steam_id);
-	
-	// see if the time exists
-	let time = await TimeModel.findOne({
-		level: level._id,
-		author: author._id,
-	});
-	if (!time) {
-		time = new TimeModel();
-		time.level = level;
-		time.author = author;
-	}
-	time.duration = data.time;
-	time.timestamp = new Date(data.Timestamp * 1000);
-	time.verified = false;
-	time.replay = filename;
-	time.save();
-	
-	level.times.push(time);
-	author.times.push(time);
-	level.save();
-	author.save();
-
-	return res.send('Success');
-});
+], TimeController.new);
 
 export default times;
