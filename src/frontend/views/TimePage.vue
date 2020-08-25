3cf4ecffe59501cd24a37c6e0658732dcd51064f
Sarah Ohlin <sarahohlin1@gmail.com>
2020-08-25 00:58:51 -0400

    feat: skeleton page for viewing a single time

---
+++ src/frontend/views/TimePage.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+	{{ this.$route.params.id }}
+</div>
+</template>
+
+<script>
+export default {
+	name: 'TimePage',
+	props: {
+		title: String,
+	},
+	created() {
+		this.$emit('child-init', this.title);
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
