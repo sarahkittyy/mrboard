21c662f18442c8196fb387df44a75e101147939b
Sarah Ohlin <sarahohlin1@gmail.com>
2020-08-25 00:57:35 -0400

    feat: submit time button on the home page

--- src/frontend/views/Home.vue
+++ src/frontend/views/Home.vue
@@ -1,6 +1,19 @@
 <template>
 <div>
-	<v-banner class="text-h5 text-center">Recent Times</v-banner>
+	<v-banner class="text-h5 text-center">
+		<v-row align="center" justify="end">
+			<v-col />
+			<v-col>
+				<span class="absolute-center">Recent Times</span>
+			</v-col>
+			<v-col class="py-0 my-0">
+				<v-btn @click="$router.push('/times/submit')" color="primary">
+					<v-icon class="mr-2">mdi-cloud-upload</v-icon>
+					<span class="text-center">Submit Time</span>
+				</v-btn>
+			</v-col>
+		</v-row>
+	</v-banner>
 	<time-card-carousel :times="$store.getters.recentTimes" />
 </div>
 </template>
