cb103722e52d8508014f0b8a6d8d295aa7696b0f
Sarah Ohlin <sarahohlin1@gmail.com>
2021-01-14 19:58:15 -0500

    fix: hide site title when width is small

--- src/frontend/components/AppBar.vue
+++ src/frontend/components/AppBar.vue
@@ -1,17 +1,27 @@
 <template>
-  <v-app-bar app fixed color="primary">
-    <v-app-bar-nav-icon @click="$emit('open-drawer');" />
-      <v-img class="clickable mr-4" @click="toSteamPage()" max-width="40" contain alt="logo" src="/assets/mr-logo.webp" />
-        <v-toolbar-title color="white">marble race community boards [beta]</v-toolbar-title>
-        <v-spacer />
-          <div class="text-h5 absolute-center">{{ title }}</div>
-          <v-spacer />
-            <profile-badge />
-    </v-app-bar>
+<v-app-bar app fixed color="primary">
+  <v-app-bar-nav-icon @click="$emit('open-drawer')"></v-app-bar-nav-icon>
+  <v-img 
+    class="clickable mr-4"
+    @click="toSteamPage()"
+    max-width="40"
+    contain
+    alt="logo"
+    src="/assets/mr-logo.webp"></v-img>
+  <v-toolbar-title v-if="showTitle" color="white">
+    marble race community boards [beta]
+  </v-toolbar-title>
+  <v-spacer></v-spacer>
+  <div class="text-h5 absolute-center">{{ title }}</div>
+  <v-spacer></v-spacer>
+  <profile-badge></profile-badge>
+</v-app-bar>
 </template>
 
 <script>
 import ProfileBadge from '~/ProfileBadge';
+
+import debounce from 'debounce';
 
 export default {
   name: 'AppBar',
@@ -21,14 +31,31 @@ export default {
       required: true
     },
   },
+  data: () => ({
+    width: window.innerWidth,
+  }),
   components: {
     ProfileBadge,
+  },
+  mounted() {
+    window.addEventListener('resize', this.recomputeWidth);
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.recomputeWidth);
   },
   methods: {
     toSteamPage() {
       document.location = 'https://store.steampowered.com/app/851640/Marble_Race/';
     },
-  }
+    recomputeWidth: debounce(function () {
+      this.width = window.innerWidth;
+    }, 400),
+  },
+  computed: {
+    showTitle() {
+      return this.width > 950;
+    },
+  },
 };
 </script>
 
