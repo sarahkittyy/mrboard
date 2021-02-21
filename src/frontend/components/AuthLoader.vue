da21e7c4c1c8cde1741a2eaa1663c38309a0260b
Sarah Ohlin <sarahohlin1@gmail.com>
2021-02-21 13:48:17 -0500

    refactor: abstract out authentication loading

---
+++ src/frontend/components/AuthLoader.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <slot v-if="!ready" name="loading"></slot>
+  <slot v-else-if="ready && !authed" name="unauthorized"></slot>
+  <slot v-else name="content"></slot>
+</div>
+</template>
+<script>
+export default {
+  name: 'AuthLoader',
+  data: () => ({
+
+  }),
+  props: {
+    level: {
+      type: Number,
+      default: 1,
+    },
+  },
+  computed: {
+    ready() {
+      return !this.$store.state.auth.loading;
+    },
+    authed() {
+      let lvl = this.$store.getters.myAuthLevel;
+      let stat = this.$store.state.auth.status;
+      return stat && (lvl >= this.level);
+    }
+  },
+};
+</script>
+<style lang="scss" scoped>
+  
+</style>
--- src/frontend/views/TimeForm.vue
+++ src/frontend/views/TimeForm.vue
@@ -2,26 +2,38 @@
   <v-container fill-height fluid>
     <v-row align="center" justify="center">
       <v-sheet elevation="5" width="50%" class="text-center pa-13">
-        <template v-if="authorized">
-          <span class="text-h6">Submit</span>
-          <v-tooltip top>
-            <template v-slot:activator="{ on }">
-              <v-icon class="ml-4 mb-1" v-on="on">mdi-folder</v-icon>
-            </template>
-            <span>
-              Replays are located at: 
-              <pre>%userprofile%/AppData/LocalLow/Elastic Sea/Marble Race/Replays</pre>
-            </span>
-          </v-tooltip>
-          <v-form ref="form" @submit="submit">
-            <v-file-input v-model="file" show-size :rules="rules" label="Any replay .rpl file" />
+        <auth-loader :level="1">
+          <template v-slot:loading>
+            <v-progress-circular indeterminate />
+          </template>
+
+          <template v-slot:unauthorized>
+            <div class="text-h5 ma-5">You must be logged in to submit a replay!</div>
+            <v-btn @click="$router.push('/')">Home</v-btn>
+          </template>
+
+          <template v-slot:content>
+            <span class="text-h6">Submit</span>
+            <v-tooltip top>
+              <template v-slot:activator="{ on }">
+                <v-icon class="ml-4 mb-1" v-on="on">mdi-folder</v-icon>
+              </template>
+              <span>
+                Replays are located at: 
+                <pre>%userprofile%/AppData/LocalLow/Elastic Sea/Marble Race/Replays</pre>
+              </span>
+            </v-tooltip>
+            <v-form ref="form" @submit="submit">
+              <v-file-input
+                v-model="file"
+                show-size
+                :rules="rules"
+                label="Any replay .rpl file"
+              />
               <v-btn type="submit" :disabled="file == null || !formValid">Submit</v-btn>
-          </v-form>
-        </template>
-        <template v-else>
-          <div class="text-h5 ma-5">You must be logged in to submit a replay!</div>
-          <v-btn @click="$router.push('/')">Home</v-btn>
-        </template>
+            </v-form>
+          </template>
+        </auth-loader>
       </v-sheet>
     </v-row>
   </v-container>
@@ -29,6 +41,7 @@
 
 <script>
 import AppBar from '~/AppBar';
+import AuthLoader from '~/AuthLoader';
 
 import FormData from 'form-data';
 
@@ -45,6 +58,7 @@ export default {
   }),
   components: {
     AppBar,
+    AuthLoader,
   },
   props: {
     title: String,
@@ -76,11 +90,6 @@ export default {
     }
   },
   computed: {
-    authorized() {
-      let a = this.$store.state.auth.status;
-      let b = this.$store.state.auth.loading;
-      return a && !b;
-    },
     doneUploading() {
       return !this.$store.state.times.uploading && this.$store.state.times.uploadStatus;	
     }
