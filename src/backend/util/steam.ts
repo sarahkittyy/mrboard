0f1157dc818f4e84f159739738343effd3561fa0
Sarah Ohlin <sarahohlin1@gmail.com>
2021-03-06 16:22:47 -0500

    refactor: remove some console.logs

--- src/backend/util/steam.ts
+++ src/backend/util/steam.ts
@@ -3,12 +3,10 @@ import fetch from 'node-fetch';
 import cheerio from 'cheerio';
 
 export async function getThumbnailURL(workshop_id: string): Promise<string> {
-  console.log('NYA\nNYA\nNYA');
   let res = await fetch(`https://steamcommunity.com/sharedfiles/filedetails/?id=${encodeURIComponent(workshop_id)}`);
   let text = await res.text();
 
   const $ = cheerio.load(text);
   let url = $('#previewImage').attr('src') ?? $('#previewImageMain').attr('src');
-  console.log(`URL --- ${url}`);
   return url;
 }
