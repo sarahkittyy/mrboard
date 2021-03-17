9bdf5b89270ac1651526c0cf37d2bfc181c591fe
Sarah Ohlin <sarahohlin1@gmail.com>
2021-03-17 19:03:24 -0400

    fix: add component used in ui pinning

---
+++ src/frontend/components/TimePinButton.vue
@@ -0,0 +1,43 @@
+<template>
+<v-tooltip top>
+  <template v-slot:activator="{ on }">
+    <v-btn v-on="on" icon @mousedown.stop @mouseover.stop @click.stop="togglePin">
+      <v-icon v-if="pinned">mdi-pin-outline</v-icon>
+      <v-icon v-else>mdi-pin</v-icon>
+    </v-btn>
+  </template>
+  <span v-if="!pinned">pin time</span>
+  <span v-else>unpin time</span>
+</v-tooltip>
+</template>
+<script>
+export default {
+  name: 'TimePinButton',
+  props: {
+    time: Object,
+  },
+  methods: {
+    pin() {
+      this.$store.dispatch('pin', this.time.id);
+    },
+    unpin() {
+      this.$store.dispatch('unpin', this.time.id);
+    },
+    togglePin() {
+      if (this.pinned) {
+        this.unpin();
+      } else {
+        this.pin();
+      }
+    },
+  },
+  computed: {
+    pinned() {
+      return this.time.pinned;
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+  
+</style>
